85bec00f310b2920c83f2d40bdb857a5bc442298
Hakimaamu4p <amu02934@gmail.com>
2024-07-18 01:35:27 +0000

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["100027867581039",];
+		const permission = ["61557094816783",];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage(" You don't have permission to use this command. 🐤", event.threadID, event.messageID);
 		}
