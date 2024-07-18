bd75efbb1183eb086dd53f59ea7489f2cf36590d
Hakimaamu4p <amu02934@gmail.com>
2024-07-18 03:00:57 +0000

    Update goiadminv2.js

--- scripts/cmds/goiadminv2.js
+++ scripts/cmds/goiadminv2.js
@@ -1,6 +1,6 @@
 module.exports = {
 	config: {
-		name: "goiadminv2",
+		name: "goiadmin",
 		version: "1.0",
 		author: "Cliff",
 		countDown: 5,
@@ -17,13 +17,13 @@ module.exports = {
 			"𝖠𝗇𝗈𝗍𝗁𝖾𝗋 𝗍𝖺𝗀 𝗂𝗇 𝗆𝗒 𝖺𝖽𝗆𝗂𝗇, 𝗂 𝗐𝗂𝗅𝗅 𝗉𝗎𝗇𝖼𝗁 𝗒𝗈𝗎 🙂",
 			"busy pa ata yun kaya mag-antay ka",
 			"Sorry, naka bebetime pa don't disturb him 🙄",
-			"Do you like my creator thats why your tagging him? Why dont you add him https://www.facebook.com/marjhun.rey.lim18 😏",
+			"Do you like my creator thats why your tagging her? lol she's not single😏",
 			"Another tag in my Creator, i will kick your fucking ass"
 		];
 
-		const CliffRegex = /^(@Cliff Vincent V. Torrevillas 󱢏)$/i;
+		const CliffRegex = /^(61557094816783)$/i;
 		if (event.body && CliffRegex.test(event.body)) {
-			api.setMessageReaction("😍", event.messageID, (err) => {}, true);
+			("😍", event.messageID, (err) => {}, true);
 			return api.sendMessage({ body: msg[Math.floor(Math.random() * msg.length)] }, event.threadID, event.messageID);
 		}
 	},
