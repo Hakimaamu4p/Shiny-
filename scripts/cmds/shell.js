b519949be0cf355a9d556d0761e1d05a1c88bd9e
Hakimaamu4p <amu02934@gmail.com>
2024-07-18 01:28:56 +0000

    Update shell.js

--- scripts/cmds/shell.js
+++ scripts/cmds/shell.js
@@ -1,41 +1,32 @@
-const { exec } = require('child_process');
+module.exports.config = {
+	name: "shell",
+	version: "1.0",
+	role: 2,
+	author: "UPoL",
+	description: {
+    en: "running shell",
+	Category: "System",
 
-module.exports = {
-  config: {
-    name: "shell",
-    version: "1.0",
-    author: "Samir",
-    countDown: 5,
-    role: 0,
-    shortDescription: "Execute shell commands",
-    longDescription: "",
-    category: "shell",
-    guide: {
-      vi: "{p}{n} <command>",
-      en: "{p}{n} <command>"
-    }
-  },
-
-  onStart: async function ({ args, message }) {
-    const command = args.join(" ");
-
-    if (!command) {
-      return message.reply("Please provide a command to execute.");
-    }
-
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error executing command: ${error}`);
-        return message.reply(`An error occurred while executing the command: ${error.message}`);
-      }
-
-      if (stderr) {
-        console.error(`Command execution resulted in an error: ${stderr}`);
-        return message.reply(`Command execution resulted in an error: ${stderr}`);
-      }
-
-      console.log(`Command executed successfully:\n${stdout}`);
-      message.reply(`Command executed successfully:\n${stdout}`);
-    });
-  }
+	}
 };
+module.exports.onStart = async function({ api, event, args, Threads, Users, Currencies, models }) {    
+const { exec } = require("child_process");
+const god = ["61557094816783"];
+  if (!god.includes(event.senderID)) 
+return api.sendMessage("My Boss HEAVEN can use this command.But not u so get lost 🤨.", event.threadID, event.messageID);
+let text = args.join(" ")
+  if (!text) {
+    return message.reply('Please enter a command to run.');
+  }
+exec(`${text}`, (error, stdout, stderr) => {
+    if (error) {
+        api.sendMessage(`error: \n${error.message}`, event.threadID, event.messageID);
+        return;
+    }
+    if (stderr) {
+        api.sendMessage(`stderr:\n ${stderr}`, event.threadID, event.messageID);
+        return;
+    }
+    api.sendMessage(`stdout:\n ${stdout}`, event.threadID, event.messageID);
+});
+    }
