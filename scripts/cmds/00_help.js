2c14dafabc0525e65b63f7ea7d41a54eaaa8f53e
Hakimaamu4p <amu02934@gmail.com>
2024-07-18 02:17:04 +0000

    Update 00_help.js

--- scripts/cmds/00_help.js
+++ scripts/cmds/00_help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "𝗔𝘀𝘁𝗿𝗮𝗹 𝗔𝗜";
+const doNotDelete = "Heaven Bot";
 /** 
 * @author NTKhang
 * @author: do not delete it
