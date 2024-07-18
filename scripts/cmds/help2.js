abf13d68ebdd29d96db5c4243fc4bf9dcfffcb9b
Hakimaamu4p <amu02934@gmail.com>
2024-07-18 02:21:11 +0000

    Update help2.js

--- scripts/cmds/help2.js
+++ scripts/cmds/help2.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 𝗔𝘀𝘁𝗿𝗮𝗹 𝗔𝗜 ]"; // changing this wont change the goatbot V2 of list cmd it is just a decoyy
+const doNotDelete = "[🧘‍♀️Heaven Bot🧘‍♀️]"; // changing this wont change the goatbot V2 of list cmd it is just a decoyy
 
 module.exports = {
 	config: {
